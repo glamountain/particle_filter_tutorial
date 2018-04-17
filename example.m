c841b244066bff2ce36599d86388ee12b57ef891
Gerald LaMountain <lamountain.g@husky.neu.edu>
2018-04-17 13:26:40 -0400

    WiP

--- example.m
+++ example.m
@@ -2,28 +2,33 @@
 clear, clc, close all;
 
 %% Process equation x[k] = sys(k, x[k-1], u[k]);
-nx = 1;  % number of states
-sys = @(k, xkm1, uk) xkm1/2 + 25*xkm1/(1+xkm1^2) + 8*cos(1.2*k) + uk; % (returns column vector)
+nx = 9;  % number of states
+F = [1 0 0 1 0 0 0.5 0 0; 0 1 0 0 1 0 0 0.5 0; 0 0 1 0 0 1 0 0 0.5; ...
+     0 0 0 1 0 0 1.0 0 0; 0 0 0 0 1 0 0 1.0 0; 0 0 0 0 0 1 0 0 1.0; ...
+     0 0 0 0 0 0 1.0 0 0; 0 0 0 0 0 0 0 1.0 0; 0 0 0 0 0 0 0 0 1.0;];
+sys = @(k, xkm1, uk) F * xkm1 + uk; % (returns column vector)
 
 %% Observation equation y[k] = obs(k, x[k], v[k]);
-ny = 1;                                           % number of observations
-obs = @(k, xk, vk) xk^2/20 + vk;                  % (returns column vector)
+ny = 3;                                           % number of observations
+obs = @(k, xk, vk) xk(1:3) + vk;                  % (returns column vector)
 
 %% PDF of process noise and noise generator function
-nu = 1;                                           % size of the vector of process noise
-sigma_u = sqrt(10);
-p_sys_noise   = @(u) normpdf(u, 0, sigma_u);
-gen_sys_noise = @(u) normrnd(0, sigma_u);         % sample from p_sys_noise (returns column vector)
+nu = nx;                                          % size of the vector of process noise                           % size of the vector of observation noise
+mu_u          = zeros(nu,1);
+sigma_u       = sqrt(0.01)*eye(nu);
+p_sys_noise   = @(u) mvnpdf(u, mu_u, sigma_u);
+gen_sys_noise = @(u) mvnrnd(mu_u, sigma_u)';       % sample from p_sys_noise (returns column vector)
 
 %% PDF of observation noise and noise generator function
-nv = 1;                                           % size of the vector of observation noise
-sigma_v = sqrt(1);
-p_obs_noise   = @(v) normpdf(v, 0, sigma_v);
-gen_obs_noise = @(v) normrnd(0, sigma_v);         % sample from p_obs_noise (returns column vector)
+nv = ny;                                          % size of the vector of observation noise
+mu_v          = zeros(nv,1);
+sigma_v       = diag(sqrt([1; 1; 1]));
+p_obs_noise   = @(v) mvnpdf(v, mu_v, sigma_v);
+gen_obs_noise = @(v) mvnrnd(mu_v, sigma_v)';       % sample from p_obs_noise (returns column vector)
 
 %% Initial PDF
 % p_x0 = @(x) normpdf(x, 0,sqrt(10));             % initial pdf
-gen_x0 = @(x) normrnd(0, sqrt(10));               % sample from p_x0 (returns column vector)
+gen_x0 = @(x) 0 + mvnrnd(mu_u, sigma_u)';          % sample from p_x0 (returns column vector)
 
 %% Transition prior PDF p(x[k] | x[k-1])
 % (under the suposition of additive process noise)
@@ -41,8 +46,8 @@ x = zeros(nx,T);  y = zeros(ny,T);
 u = zeros(nu,T);  v = zeros(nv,T);
 
 %% Simulate system
-xh0 = 0;                                  % initial state
-u(:,1) = 0;                               % initial process noise
+xh0    = zeros(nx,1);                     % initial state
+u(:,1) = zeros(nu,1);                     % initial process noise
 v(:,1) = gen_obs_noise(sigma_v);          % initial observation noise
 x(:,1) = xh0;
 y(:,1) = obs(1, xh0, v(:,1));
@@ -77,6 +82,7 @@ for k = 2:T
    [xh(:,k), pf] = particle_filter(sys, y(:,k), pf, 'systematic_resampling');   
  
    % filtered observation
+   obs(k, xh(:,k), 0)
    yh(:,k) = obs(k, xh(:,k), 0);
 end
 
@@ -109,16 +115,16 @@ title('Evolution of the state density','FontSize',14)
 %% plot of the state vs estimated state by the particle filter vs particle paths
 figure
 hold on;
-h1 = plot(1:T,squeeze(pf.particles),'y');
-h2 = plot(1:T,x,'b','LineWidth',4);
-h3 = plot(1:T,xh,'r','LineWidth',4);
-h4 = plot(1:T,xhmode,'g','LineWidth',4);
+h1 = plot(1:T,squeeze(pf.particles(1,:,:)),'y');
+h2 = plot(1:T,x(1,:),'b','LineWidth',4);
+h3 = plot(1:T,xh(1,:),'r','LineWidth',4);
+h4 = plot(1:T,xhmode(1,:),'g','LineWidth',4);
 legend([h2 h3 h4 h1(1)],'state','mean of estimated state','mode of estimated state','particle paths');
 title('State vs estimated state by the particle filter vs particle paths','FontSize',14);
 
 %% plot of the observation vs filtered observation by the particle filter
 figure
-plot(1:T,y,'b', 1:T,yh,'r');
+plot(1:T,y(1,:),'b', 1:T,yh(1,:),'r');
 legend('observation','filtered observation');
 title('Observation vs filtered observation by the particle filter','FontSize',14);
 
